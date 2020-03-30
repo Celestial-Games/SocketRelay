b4c40d7a500a7f530bb02cfb375cbf4e7853c243
tbulford <travis@celestial-games.com>
2020-03-30 16:56:59 +0200

    Checed how much data the text area will show for logging, this will save memory.

--- SocketRelayClient/src/main/java/com/socketrelay/client/swing/TextAreaOutputStream.java
+++ SocketRelayClient/src/main/java/com/socketrelay/client/swing/TextAreaOutputStream.java
@@ -3,9 +3,18 @@ package com.socketrelay.client.swing;
 import java.io.OutputStream;
 
 import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TextAreaOutputStream extends OutputStream {
+	private static final Logger logger=LoggerFactory.getLogger(TextAreaOutputStream.class);
     private JTextArea textArea;
+    
+    private boolean running=true;
+    
+    private final static int SCROLL_BUFFER_SIZE=1000;
     
     public TextAreaOutputStream(JTextArea textArea) {
         this.textArea = textArea;
@@ -13,7 +22,20 @@ public class TextAreaOutputStream extends OutputStream {
      
     @Override
     public void write(int b) {
-        textArea.append(String.valueOf((char)b));
-        textArea.setCaretPosition(textArea.getDocument().getLength());
+    	if (running) {
+	        textArea.append(String.valueOf((char)b));
+	        
+	        int extraLines = textArea.getLineCount() - SCROLL_BUFFER_SIZE;
+	        if(extraLines > 0) {
+	            try {
+	                int pos = textArea.getLineEndOffset(extraLines - 1);
+	                textArea.replaceRange("",0,pos);
+	            } catch (BadLocationException e) {
+	            	running=false;
+	            	logger.error(e.getMessage(),e);
+	            }
+	        }
+	        textArea.setCaretPosition(textArea.getDocument().getLength());
+    	}        
     }
 }
